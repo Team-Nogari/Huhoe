3d2734d207c2dd9323cf6f7474355939009fa1be
yim2627 <yim2627@gmail.com>
2022-04-06 20:42:48 +0900

    feat: 프로젝트 설정 변경 SwiftUI -> UIKit

---
+++ Huhoe/Huhoe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Huhoe
+//
+//  Created by 임지성 on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
