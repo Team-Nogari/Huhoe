7c46d991504f358280bafc1bac413fed4bce6027
yim2627 <yim2627@gmail.com>
2022-04-06 15:04:39 +0900

    feat: 프로젝트 생성

---
+++ Huhoe/Huhoe/HuhoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuhoeApp.swift
+//  Huhoe
+//
+//  Created by 임지성 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct HuhoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
