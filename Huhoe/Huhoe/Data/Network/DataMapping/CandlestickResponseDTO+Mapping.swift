929bcf5de0319953b9a0f6f85a477d7f70ddbfda
HJEHA <hyhpwang4@gmail.com>
2022-04-08 00:18:00 +0900

    feat: Candlestick DTO 구현 #2

---
+++ Huhoe/Huhoe/Data/Network/DataMapping/CandlestickResponseDTO+Mapping.swift
@@ -0,0 +1,13 @@
+//
+//  CandlestickResponseDTO+Mapping.swift
+//  Huhoe
+//
+//  Created by 황제하 on 2022/04/07.
+//
+
+import Foundation
+
+struct CandlestickResponseDTO {
+    let status: String
+    let candlestickDTO: [[Any]]
+}
