5cbcab71ec55367426b318121d5c529b8bf8f2e7
Jiseong <yim2627@gmail.com>
2022-04-07 18:55:54 +0900

    feat: NetworkProvider 생성 #3
    - Network 파일 구조 생성
    - endPoint(baseURL) 설정

---
+++ Huhoe/Huhoe/Data/Network/NetworkProvider.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkProvider.swift
+//  Huhoe
+//
+//  Created by 임지성 on 2022/04/07.
+//
+
+import Foundation
+
+final class NetworkProvider {
+    private let apiEndPoint: String
+    
+    init() {
+        apiEndPoint = "https://api.bithumb.com/public"
+    }
+}
