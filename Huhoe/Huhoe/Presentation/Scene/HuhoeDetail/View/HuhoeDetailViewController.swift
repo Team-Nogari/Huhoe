8c7ffa9babbe4493725811b6cd2f542ae1f86390
HJEHA <hyhpwang4@gmail.com>
2022-04-23 11:59:03 +0900

    feat: 디테일 뷰 IBOutlet 연결 #13

--- Huhoe/Huhoe/Presentation/Scene/HuhoeDetail/View/HuhoeDetailViewController.swift
+++ Huhoe/Huhoe/Presentation/Scene/HuhoeDetail/View/HuhoeDetailViewController.swift
@@ -8,18 +8,33 @@
 import UIKit
 
 final class HuhoeDetailViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet private weak var currentPriceLabel: UILabel!
+    @IBOutlet private weak var dateChangeButton: UIButton!
+    @IBOutlet private weak var moneyTextField: UITextField!
+    @IBOutlet private weak var pastPriceLabel: UILabel!
+    @IBOutlet private weak var pastQuantityLabel: UILabel!
+    @IBOutlet private weak var coinHistoryCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureBackButton()
+        configureDateChangeButton()
     }
 }
 
-// MARK: - Configure Navigation Bar
+// MARK: - Configure View
 
 extension HuhoeDetailViewController {
     private func configureBackButton() {
         navigationController?.navigationBar.tintColor = .label
         navigationController?.navigationBar.topItem?.title = String()
+    }
+    
+    private func configureDateChangeButton() {
+        dateChangeButton.layer.cornerRadius = 6
     }
 }
