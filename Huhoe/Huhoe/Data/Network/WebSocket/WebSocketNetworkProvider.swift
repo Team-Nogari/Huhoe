1ce03b9b6ef446e2bf781c6b84f03b7ad5e90ad6
Jiseong <yim2627@gmail.com>
2022-04-25 20:00:41 +0900

    feat: WebSocketNetworkProvider 구현 #12
    - APIPath 경로 수정

---
+++ Huhoe/Huhoe/Data/Network/WebSocket/WebSocketNetworkProvider.swift
@@ -0,0 +1,16 @@
+//
+//  WebSocketNetworkProvider.swift
+//  Huhoe
+//
+//  Created by 임지성 on 2022/04/25.
+//
+
+import Foundation
+
+final class WebSocketNetworkProvider {
+    private let apiEndPoint: String
+    
+    init() {
+        self.apiEndPoint = "wss://pubwss.bithumb.com/pub/ws"
+    }
+}
