10c00eba364fdcf9ab780b16a73bed175a72f380
Jiseong <yim2627@gmail.com>
2022-05-15 16:34:38 +0900

    feat: HuhoeAssistViewController 추가 #53

---
+++ Huhoe/Huhoe/Presentation/Scene/HuhoeAssist/HuhoeAssistViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HuhoeAssistViewController.swift
+//  Huhoe
+//
+//  Created by 임지성 on 2022/05/15.
+//
+
+import UIKit
+
+class HuhoeAssistViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
